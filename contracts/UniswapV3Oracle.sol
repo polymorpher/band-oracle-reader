79ff982d741308e302f96a5e49b3c2e5627bed5e
polymorpher <aaron@qokka.ai>
2024-08-13 22:09:20 -0700

    fix decimal issues for uniswapv3oracle

--- contracts/UniswapV3Oracle.sol
+++ contracts/UniswapV3Oracle.sol
@@ -3,13 +3,18 @@ pragma experimental ABIEncoderV2;
 
 import {SynthOracle} from "./SynthOracle.sol";
 import {IUniswapV3Pool} from "v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import {ERC20Detailed} from "openzeppelin-contracts/token/ERC20/ERC20Detailed.sol";
 
-contract BandOracleReader is SynthOracle {
+contract UniswapV3SpotOracle is SynthOracle {
     IUniswapV3Pool public pool;
     bool public inverse;
-    uint256 token0Decimals;
-    uint256 token1Decimals;
+    uint8 public token0Decimals;
+    uint8 public token1Decimals;
 
+    /// by default, price is token1/token0, i.e. number of token1 needed to buy 1 unit of token0.
+    /// e.g. For USDC/WETH pair, the default price is the number of WETH needed to buy 1 USDC
+    /// when _inverse is set to true, the oracle will return price as token1/token0 instead
+    /// the price returned by this oracle is in decimal format (e.g. 1234.567 means price is $1234.567 per WETH) multiplied by 1e18 for precision.
     constructor(
         IUniswapV3Pool _pool,
         bool _inverse,
@@ -20,12 +25,19 @@ contract BandOracleReader is SynthOracle {
         inverse = _inverse;
         updateFee = _updateFee;
         address token0 = _pool.token0();
-        token0Decimals = IERC20Minimal(token0)
+        address token1 = _pool.token1();
+        token0Decimals = ERC20Detailed(token0).decimals();
+        token1Decimals = ERC20Detailed(token1).decimals();
     }
 
     function _readUniswapPrice() internal returns (uint256){
         (uint160 sqrtPriceX96,,,,,,) = pool.slot0();
         uint256 price = (uint256(sqrtPriceX96) * uint256(sqrtPriceX96) * 1e18) >> 192;
+        if (token0Decimals > token1Decimals) {
+            price = price * (10 ** (token0Decimals - token1Decimals));
+        } else if (token0Decimals < token1Decimals) {
+            price = price / (10 ** (token1Decimals - token0Decimals));
+        }
         if (inverse) {
             price = 1e18 / price;
         }
