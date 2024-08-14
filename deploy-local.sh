d99b2ded456ebaec8b1b4593161a3251174efbb5
polymorpher <aaron@qokka.ai>
2024-08-14 02:49:50 -0700

    separate deploy script for local and harmony prod; env example

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-export $(grep -v '^#' .env | xargs)
+export $(grep -v '^#' .env.deploy-local | xargs)
 forge script deploy/UniswapV3SpotOracle.s.sol --rpc-url http://127.0.0.1:8545 \
   --chain-id 1 \
   --broadcast \
---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+export $(grep -v '^#' .env | xargs)
+forge script deploy/UniswapV3SpotOracle.s.sol --rpc-url https://api.harmony.one \
+  --chain-id 1666600000 \
+  --gas-price 100000000000 \
+  --legacy \
+  --broadcast \
+  --private-key ${DEPLOYER_PRIVATE_KEY} \
+  -vv
+
